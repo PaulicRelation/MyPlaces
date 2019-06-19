e3b3b1841e7389a5dad9e0a0d21a5681d9e88aca
Pavel <pavel.senchenko@gmail.com>
2019-06-19 17:31:08 +0300

    Get dress from map

--- MyPlaces/Controllers/MapViewController.swift
+++ MyPlaces/Controllers/MapViewController.swift
@@ -10,19 +10,40 @@ import UIKit
 import MapKit
 import CoreLocation
 
+protocol MapViewControllerDelegate {
+    func getAddress(_ adress: String?)
+}
+
 class MapViewController: UIViewController {
 
+
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapPinImmage: UIImageView!
+    @IBOutlet weak var currentAddressLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
 
     var place = Place()
+    var mapViewControllerDelegate: MapViewControllerDelegate?
+
     let annotationIdentifier  = "annotationIdentifier"
     let locationManager = CLLocationManager()
+    var incomeSegueIdentifier = ""
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        setupPlacemark()
+        setupMapView()
         checkLocationServices()
+    }
+
+    private func setupMapView() {
+        if incomeSegueIdentifier == "showPlace" {
+            mapPinImmage.isHidden = true
+            currentAddressLabel.isHidden = true
+            doneButton.isHidden = true
+            setupPlacemark()
+        }
     }
 
     private func setupPlacemark() {
@@ -79,9 +100,10 @@ class MapViewController: UIViewController {
                       message: "To give permission go: Settings -> MyPlaces -> Location"
             )
         case .authorizedAlways:
-            break 
+            break
         case .authorizedWhenInUse:
             mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "GetAddress" { showUserLocation() }
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         @unknown default:
@@ -98,15 +120,28 @@ class MapViewController: UIViewController {
         }
     }
 
-    @IBAction func shiftToUserLocation(_ sender: Any) {
+    private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+
+    private func showUserLocation() {
         if let location = locationManager.location?.coordinate {
             let region = MKCoordinateRegion(center: location, latitudinalMeters: 1000, longitudinalMeters: 1000)
             mapView.setRegion(region, animated: true)
-
         }
+    }
+
+    @IBAction func shiftToUserLocation(_ sender: Any) {
+        showUserLocation()
     }
     @IBAction func closeVC(_ sender: Any) {
         dismiss(animated: true)
+    }
+    @IBAction func doneButtonPressed() {
+        mapViewControllerDelegate?.getAddress(currentAddressLabel.text)
+        dismiss(animated: true) 
     }
 }
 
@@ -130,6 +165,28 @@ extension MapViewController: MKMapViewDelegate {
             annotationView?.rightCalloutAccessoryView = imageView
         }
         return  annotationView
+    }
+
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geocoder = CLGeocoder()
+
+        geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
+            if let error = error { print(error); return }
+            guard let placemarks = placemarks else { return }
+
+            let placemark = placemarks.first
+            let streetName = placemark?.thoroughfare
+            let buildNumber = placemark?.subThoroughfare
+
+            DispatchQueue.main.async {
+                if streetName != nil && buildNumber != nil {
+                    self.currentAddressLabel.text = "\(streetName!), \(buildNumber!)"
+                } else if streetName != nil {
+                    self.currentAddressLabel.text  = "\(streetName!)"
+                } else { self.currentAddressLabel.text = "" }
+            }
+        }
     }
 }
 
